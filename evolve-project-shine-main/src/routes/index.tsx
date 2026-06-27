371b0fd857016f046ec5b80715180d1fc6a3a5a9
krystiankorsik28-art <krystiankorsik28@gmail.com>
2026-06-27 13:49:37 +0200

    Switch homepage to enterprise landing

--- evolve-project-shine-main/src/routes/index.tsx
+++ evolve-project-shine-main/src/routes/index.tsx
@@ -1,10 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { PremiumLanding } from "@/components/landing/PremiumLanding";
-
-function LandingPage() {
-  return <PremiumLanding />;
-}
+import { EnterpriseLanding } from "@/components/landing/EnterpriseLanding";
 
 export const Route = createFileRoute("/")({
-  component: LandingPage,
+  component: EnterpriseLanding,
 });
